03f57484445487eb19b35517527ed9845694759f
HuggableSquare <huggablesquare@gmail.com>
2013-11-17 21:07:46 -0500

    cleaner output, more input options

--- console.cpp
+++ console.cpp
@@ -4,47 +4,38 @@
 
 using namespace std;
 char choice;
-
+int range;
 void menu()
 {
-	
-	cout << "Press enter to start.";
-	cin.get();
+	cout << "Enter the the range of numbers you want to guess between.";
+	cin >> range;
 	
 }
-
-
 void game()
 {
 	int guess, real, tries;
 	tries = 1;
 	srand(unsigned int(time(NULL)));
-	real = rand() % 10 + 1;
-	cout << "Guess a number from 1 - 10. ";
+	real = rand() % range + 1;
+	cout << "Guess a number from 1 - " << range << ". ";
 	cin >> guess;
 	while(guess != real)
 	{
 		if(guess>real)
 		{
-			cout << "Lower!" << endl;
+			cout << "Lower!" << endl << endl;
 			cin >> guess;
 		}
 		if(guess<real)
 		{
-			cout << "Higher!" << endl;
+			cout << "Higher!" << endl << endl;
 			cin >> guess;
 		}
 		tries++;
 	}
 
 	cout << "You win!" << endl << "It took you " << tries << " tries to guess the number!" << endl;
-	//return 0;
 }
-			
-			
-			
-			
-			
 int main()
 {
 	menu();
