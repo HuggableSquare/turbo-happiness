5dc6c538e03c7bfb341ae274816d7745bb48bbb0
HuggableSquare <huggablesquare@gmail.com>
2015-08-19 23:46:17 -0400

    dromedary word competition program

---
+++ dromedary.cpp
@@ -0,0 +1,36 @@
+#include <iostream.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+	char input[22];
+	int x, y, z;
+	cout << "Enter a word: ";
+	cin.get(input, 22);
+	y = strlen(input);
+	bool d = 0;
+	for(z=0; z<y-1; z++)
+	{
+		if(input[z] == input[z+1])
+		{
+			cout << input << " is NOT a dromedary word." << endl;
+			return 0;
+		}
+		if(input[z] < input[z+1])
+		{
+			if(d != 0)
+			{
+				cout << input << " is NOT a dromedary word." << endl;
+				return 0;
+			}
+		}
+		if(input[z] > input[z+1])
+		{
+			d = 1;
+		}
+	}
+	cout << input << " is a dromedary word." << endl;
+			
+	return 0;
+}
